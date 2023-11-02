df91c3c6d066787f7f1f7c925a8f94b8d3b1071f
Denis <den@listapad@mail.ru>
2023-11-02 19:47:42 +0300

    save lesson_1.py in git

---
+++ lesson_1.py
@@ -0,0 +1,40 @@
+# some_positive_number = 4
+# SomeDriver = "Pit"
+# some_number = 5
+# time = "19.23"
+# print("some number:",some_number,"some_positive_number:", some_positive_number)
+
+# some_string = "hello"
+# some_bool = True
+# some_float_number = 3.14
+#
+# print(some_string)
+#
+# some_string = 67
+#
+# print(type(bool(some_number)))
+
+# print(type(some_number),type(some_string),type(some_bool),type(some_float_number))
+
+
+# типа данных
+
+# строки str "" ''
+# целые числа int 5 6 -521
+# вещественные/дробное числа float 4.21 5.61 -9.09
+# boolean True False 1 0 1 0
+"""
+это
+многострочный
+комментарий
+"""
+'''
++ - * /
+// %
+**
+'''
+# task 1
+print(5+2)
+
+print("second work")
+
